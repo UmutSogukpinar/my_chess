e7367342dbda3d1afcb29125ccefcc2b9e376975
UmutSogukpinar <usogukpinar0@gmail.com>
2024-12-04 14:58:54 +0300

    pawn class added frame

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -1,24 +1,38 @@
 package src.pieces;
 
 import src.chess_board.BoardSquare;
+import src.chess_board.ChessBoard;
 
+import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
 public class Pawn extends Piece
 {
-    Image whitePawnImg;
-    Image blackPawnImg;
-
     public Pawn(boolean isWhite, BoardSquare square)
     {
-        this.isWhite = isWhite;
-        this.square = square;
-        img = isWhite ? whitePawnImg : blackPawnImg;
+        super(isWhite, square);
+        img = getPieceImg(isWhite);
     }
 
     @Override
-    public void showPotentialMovement()
+    public Image getPieceImg(boolean isWhite)
     {
+        URL pawnResource = isWhite ? ChessBoard.class.getResource("../images/pieces/white/pawn.png")
+                : ChessBoard.class.getResource("../images/pieces/black/pawn.png");
+        if (pawnResource == null)
+           throw new RuntimeException(isWhite ? "Image cannot be found ../images/pieces/white/pawn.png"
+                   : "Image cannot be found ../images/pieces/black/pawn.png");
 
+        return (new ImageIcon(pawnResource).getImage());
+    }
+
+    @Override
+    public void showPotentialMovement() {}
+
+    public void drawPiece(Graphics graphics)
+    {
+        graphics.drawImage(img, position.getX_axis(), position.getY_axis(),
+                size.getX_axis(), size.getY_axis(), null);
     }
 }
--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,14 +1,43 @@
 package src.pieces;
 
+import java.awt.*;
+
+import src.basic_features.Position;
+import src.basic_features.Size;
 import src.chess_board.BoardSquare;
 
-import java.awt.*;
+import static src.chess_board.BoardSquare.squareSide;
 
 public abstract class Piece
 {
     protected boolean isWhite;
     Image img;
+    Position position;
+    static final Size size = new Size((4 * squareSide) / 5, (5 * squareSide) / 6);
     BoardSquare square;
 
+    public Piece(boolean isWhite, BoardSquare square)
+    {
+        this.isWhite = isWhite;
+        this.square = square;
+        this.position = centerThePieceBySquare();
+        square.setTherePiece(true);
+    }
+
+    // abstract methods
     public abstract void showPotentialMovement();
+    public abstract Image getPieceImg(boolean isWhite);
+
+    // non-abstract methods
+    public Position centerThePieceBySquare()
+    {
+        int squareCenterX = square.getPosition().getX_axis() + (square.getSize().getX_axis() / 2);
+        int squareCenterY = square.getPosition().getY_axis() + (square.getSize().getY_axis() / 2);
+
+        int piecePositionX = squareCenterX - (size.getX_axis() / 2);
+        int piecePositionY = squareCenterY - (size.getY_axis() / 2);
+
+        return (new Position(piecePositionX, piecePositionY));
+    }
+
 }
